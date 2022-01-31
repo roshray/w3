b3d91eea9bb973cef7e141ccfdf8356443f10589
roshray <roshanroy759@gmail.com>
2022-01-31 18:17:11 +0530

    send Transaction

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -9,7 +9,7 @@ import { TransactionContext } from "../context/TransactionContext"
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white'
 
-const Input = ({placeholder,name,type,value,handleChange}) => (
+const Input = ({ placeholder, name, type, value, handleChange}) => (
     <input 
         placeholder={placeholder}
         type={type}
@@ -20,10 +20,10 @@ const Input = ({placeholder,name,type,value,handleChange}) => (
     />
 )
 const Welcome = () => {
-    const { connectWallet, currentAccount, formData,sendTransaction, handleChange } = useContext(TransactionContext)
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext)
 
-    const handleSumit = (e) => {
-        const {addressTo, amount, keyword, message} = formData
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message} = formData
 
         e.preventDefault()
         
@@ -89,23 +89,23 @@ const Welcome = () => {
                 </div>
 
                 <div className="p-5 sm:w-96 w-full flex flex-col  jsutify-center items-center blue-glassmorphism">
-                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange}/>
-                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange}/>
-                     <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange}/>
-                     <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange}/>
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
-                     <div className="h-[1px] w-full bg-gray-400 my-2" />
+                    <div className="h-[1px] w-full bg-gray-400 my-2" />
 
                      {false ? (
                         <Loader />
                      ) : (
-                         <button
+                        <button
                             type="button"
-                            onClick={handleSumit}
+                            onClick={handleSubmit}
                             className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
-                         >
+                        >
                              Send Now
-                         </button>
+                        </button>
                      ) }
                 </div>
             </div>
