c6ca1c50c5f5177c22d3acb4f9f6d4abea191f5d
roshray <roshanroy759@gmail.com>
2022-02-02 02:39:13 +0530

    footer component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,7 @@ import { BsInfoCircle } from 'react-icons/bs'
 import { Loader } from './'
 
 import { TransactionContext } from "../context/TransactionContext"
+import { shortenAddress } from '../utils/shortenAddress'
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white'
 
@@ -79,7 +80,7 @@ const Welcome = () => {
                         </div>
                         <div>
                             <p className="text-white font-light text-sm">
-                                Address
+                                {shortenAddress(currentAccount)}
                             </p>
                             <p className="text-white font-semibold text-lg mt-1">
                                 Ethereum
